ac2e39733d3421bf90749dd154405ccbdfdaacc7
danisantosr <dsantosrivilla@gmail.com>
2021-04-07 21:37:21 +0200

    creado paquete con la escritura de la info de los devices en un csv

--- src/shopscraper/main.py
+++ src/shopscraper/main.py
@@ -1,6 +1,11 @@
 from src.shopscraper.scrapers.scraper import mediamarkt_scrap
+from src.shopscraper.write_to_csv.write_to_csv import write_to_csv
+import csv
 
 if __name__ == "__main__":
     devices = mediamarkt_scrap()
     for device in devices:
         print(str(device))
+
+    write_to_csv(devices)
+
---
+++ src/shopscraper/write_to_csv/write_to_csv.py
@@ -0,0 +1,10 @@
+import csv
+def write_to_csv(devices):
+    filename = 'devices.csv'
+
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["name", "color", "storage", "memory", "screen", "cpu", "battery", "os", "cpu_speed", "price"])
+        for device in devices:
+            writer.writerow([device.name, device.color, device.storage, device.memory, device.screen, device.cpu, \
+                             device.battery, device.os, device.cpu_speed, device.price])
